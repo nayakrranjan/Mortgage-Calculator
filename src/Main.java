3386086a918060c93ef602e062e6c82a2943196a
Rasmiranjan Nayak <rasmiranjan701@gmail.com>
2023-11-25 17:00:10 +0530

    "Version 0.3"

--- src/Main.java
+++ src/Main.java
@@ -5,43 +5,36 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        long loanAmount = (long) Main.getData("Principle: ", 100000, 10000000);
+        float annualInterest = (float) Main.getData("Annual Interest Rate: ", 0, 30);
+        short period = (short) Main.getData("Period(Years): ", 0, 15);
+
+        Double mortgage = Main.calculateMortgage(loanAmount, annualInterest, period);
+
+        System.out.print("Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
+    }
+
+    public static double getData(String prompt, int minValue, int maxValue) {
+        Scanner scanner = new Scanner(System.in);
+        double returnValue = 0;
+        while (true) {
+            System.out.print(prompt);
+            returnValue = scanner.nextDouble();
+            if (returnValue > minValue && returnValue <= maxValue)
+                return returnValue;
+            System.out.println("Please Enter a amount between " + minValue + " and " + maxValue);
+        }
+    }
+
+    public static double calculateMortgage (long loanAmount, float annualInterest, short period) {
         final byte percent = 100;
         final byte monthsInYear = 12;
-        long loanAmount = 0;
-        float annualInterest = 0;
-        byte period = 0;
-
-        while (true) {
-            System.out.print("Principle: ");
-            loanAmount = sc.nextLong();
-            if (loanAmount > 100000 && loanAmount <= 10000000)
-                break;
-            System.out.println("Please Enter a amount between 1L and 1C");
-        }
-        while (true) {
-            System.out.print("Annual Interest Rate: ");
-            annualInterest = sc.nextFloat();
-            if( annualInterest > 0 && annualInterest <= 30)
-                break;
-            System.out.println("Please Enter a amount between 1 and 30");
-        }
-        while (true) {
-            System.out.print("Period(Years): ");
-            period = sc.nextByte();
-            if( period > 0 && period <= 15)
-                break;
-            System.out.println("Please Enter a amount between 1 and 15");
-        }
 
         float monthlyInterest = annualInterest/ percent/ monthsInYear;
         int numberOfPayments = period * monthsInYear;
 
-        Double mortgage = loanAmount *
-                            (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
-                                    /(Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
-
-        System.out.print("Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
-
-
+        return loanAmount *
+                (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                /(Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
     }
 }
