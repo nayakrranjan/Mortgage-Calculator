9d5050f037f238c217eff5516123675d8d897849
Rasmiranjan Nayak <rasmiranjan701@gmail.com>
2023-11-25 14:20:34 +0530

    "Version 0.2"

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,31 @@ public class Main {
 
         final byte percent = 100;
         final byte monthsInYear = 12;
+        long loanAmount = 0;
+        float annualInterest = 0;
+        byte period = 0;
 
-        System.out.print("Principle: ");
-        long loanAmount = sc.nextLong();
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest = sc.nextFloat();
-        System.out.print("Period(Years): ");
-        byte period = sc.nextByte();
+        while (true) {
+            System.out.print("Principle: ");
+            loanAmount = sc.nextLong();
+            if (loanAmount > 100000 && loanAmount <= 10000000)
+                break;
+            System.out.println("Please Enter a amount between 1L and 1C");
+        }
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            annualInterest = sc.nextFloat();
+            if( annualInterest > 0 && annualInterest <= 30)
+                break;
+            System.out.println("Please Enter a amount between 1 and 30");
+        }
+        while (true) {
+            System.out.print("Period(Years): ");
+            period = sc.nextByte();
+            if( period > 0 && period <= 15)
+                break;
+            System.out.println("Please Enter a amount between 1 and 15");
+        }
 
         float monthlyInterest = annualInterest/ percent/ monthsInYear;
         int numberOfPayments = period * monthsInYear;
